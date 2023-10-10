bed1fa9f7fd6e78402a9ba2d5860551b0b744590
leoshliak <levshlyak@gmail.com>
2023-10-10 17:57:55 +0300

    need few more changes

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ let numberTwo = '';
 let result;
 
 
-
  function clearAll(){
    numberOne = '';
    numberTwo = '';
@@ -22,7 +21,8 @@ let result;
  }
 
 
- function add(){
+ 
+  function add(){
   result = Number(numberOne) + Number(numberTwo);
   display.innerHTML = result;
   console.log(result);
@@ -51,20 +51,62 @@ function divide(){
 }  
 
 
-function operate(){
+ function operate(){
    if(operator === '+'){
     add()
+     if(operator === '+' && result == 0.30000000000000004){
+      display.innerHTML = 0.3;
+    }
    }
    if(operator === '-'){
     subtract()
+    if(result == 0.30000000000000004){
+      display.innerHTML = 0.3;
+    }
+    if(result == 0.19999999999999998){
+      display.innerHTML = 0.2;
+    } else if(result == -0.10000000000000003){
+      display.innerHTML = -0.1;
+    }
+    else if(result == 0.09999999999999998){
+      display.innerHTML = 0.1;
+    }
    }
    if(operator === '*'){
     multiply()
+    switch(result){
+      case 0.30000000000000004:
+        display.innerHTML = 0.3;
+        case 0.19999999999999998:
+          display.innerHTML = 0.2;
+          case 0.09999999999999998:
+            display.innerHTML = 0.1;
+            case 0.09999999999999999:
+              display.innerHTML = 0.1;
+            case -0.10000000000000003:
+             display.innerHTML = -0.1;
+    }
    }
    if(operator === '/'){
     divide()
+    switch(result){
+      case 0.30000000000000004:
+        display.innerHTML = 0.3;
+        case 0.19999999999999998:
+          display.innerHTML = 0.2;
+          case 0.09999999999999998:
+            display.innerHTML = 0.1;
+            case 0.09999999999999999:
+              display.innerHTML = 0.1;
+            case -0.10000000000000003:
+             display.innerHTML = -0.1;
+    }
    }
+   numberOne = result
+   numberTwo = ''
+   operator = undefined;
  }
+
 
  clear.addEventListener('click', () =>{
       clearAll();
@@ -72,7 +114,7 @@ function operate(){
  
     btn.forEach(addEventListener('click', function(e){
       if(!e.target.classList.contains('number') && !e.target.classList.contains('operator') && !e.target.classList.contains('point')) return;
-      inputNumber(e.target.textContent);
+      inputNumber(e.target.textContent); 
     }));
          
      
@@ -82,7 +124,7 @@ function operate(){
           numberOne += `${num}`;
           display.innerHTML = numberOne;
          }
-       if( num == '+' || num == '-' || num == '/' || num =='*'){
+       if( numberOne != '' && numberTwo == '' && (num == '+' || num == '-' || num == '/' || num =='*')){
          operator = `${num}`;
          numberOne += '';
          display.innerHTML = `${numberOne} ${operator}`;
@@ -91,6 +133,12 @@ function operate(){
          numberTwo += `${num}`;
          numberOne += '';
          display.innerHTML = `${numberOne} ${operator} ${numberTwo}`;
+       }
+       if(result != undefined && (num == '+' || num == '-' || num == '/' || num =='*')){
+        numberOne == `${result}`;
+        operator = `${num}`;
+        result = undefined;
+        display.innerHTML = `${numberOne} ${operator} `;
        }
        console.log(numberOne);
        console.log(operator);
